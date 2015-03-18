7071af448a4085c9754351cf475bb8d39a3bacfd
Nick DelRossi <nickwtf@gmail.com>
2015-03-18 08:59:10 -0400

    Add more info to yaml file

--- app/models/wedding.rb
+++ app/models/wedding.rb
@@ -4,6 +4,14 @@ class Wedding
     @data = YAML.load_file("#{Rails.root}/wedding.yml")
   end
 
+  def city_and_state
+    @data.fetch('location')['city'] + ", " + @data.fetch('location')['state']
+  end
+
+  def address_for_maps
+    @data.fetch('location')['street'] + "+" + city_and_state.gsub(' ', '+')
+  end
+
   def method_missing(m, *args, &block)
     data = @data.fetch("#{m}")
 
